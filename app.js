be6d685503a98278135533d64e06038cb6d06ead
halilibrahimcelik <you@example.com>
2022-06-15 13:08:35 +0300

    Fetching features of dishes.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const singleMealElement = document.querySelector("#single-meal");
 console.log(submitBtn);
 //Event Listeners
 submitBtn.addEventListener("submit", searchMealHandler);
+mealElement.addEventListener("click", searchDishesHandler);
 
 //Functions
 function searchMealHandler(e) {
@@ -53,4 +54,65 @@ function gettingDishes(data) {
       `;
     })
     .join("");
+}
+
+//!searchDishesHandler gives us each and unique dish
+function searchDishesHandler(event) {
+  const mealInfo = event.path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+  //console.log(mealInfo);
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute("data-mealid");
+    console.log(mealId);
+    getDishbyId(mealId);
+  }
+}
+
+function getDishbyId(id) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      console.log(meal);
+      addMealFeaturesToDOM(meal);
+    });
+}
+
+function addMealFeaturesToDOM(meal) {
+  const { strMeal, strMealThumb, strCategory, strArea, strInstructions } = meal;
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    //!we check whether or not we have ingredient because some of ingredients do not exist in the API
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  singleMealElement.innerHTML = `
+ <div class="singleMeal">
+  <h2>${strMeal} </h2>
+  <img src="${strMealThumb}"  alt="${strMeal} "/>
+  <div class="single-meal-info">
+  ${strCategory ? `<p>${strCategory}</p>` : ""}
+  ${strArea ? `<p>${strArea}</p>` : ""}
+  </div>
+  <div class="main">
+  <p>${strInstructions}</p>
+  <h2>Ingredients</h2>
+  <ul>
+${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+
+  </ul>
+  </div>
+ </div>
+  `;
+  console.log(ingredients);
 }
